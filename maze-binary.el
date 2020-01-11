8638bbf476f650541a37aabf760aed9adc2888a6
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-01-11 12:28:58 +0100

    Add a binary algorithm maze

---
+++ maze-binary.el
@@ -0,0 +1,30 @@
+(require 'maze-data)
+(require 'maze-utils)
+
+(defun maze/right-cell (column row)
+  (list (1+ column) row))
+
+(defun maze/bottom-cell (column row)
+  (list column (1+ row)))
+
+(defun maze/valid-cell-p (maze cell)
+  (not (maze/error-for-cell maze cell)))
+
+(defun maze/binary--choices (maze column row)
+  (--filter (apply #'maze/valid-cell-p (list maze it))
+            (list (maze/right-cell column row)
+                  (maze/bottom-cell column row))))
+
+(defun maze/carve-binary (maze)
+  (let ((result maze))
+    (maze/for-each-cell maze
+      (let ((choice (maze/random-choice (maze/binary--choices maze column row))))
+        (when choice
+          (setq result (apply #'maze/carve-passage (cons result (list (list column row)
+                                                                 choice)))))))
+    result))
+
+(defun maze/binary (columns rows)
+  (maze/carve-binary (maze/create-empty columns rows)))
+
+(provide 'maze-binary)
