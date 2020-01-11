f4f2aa0dbb08a523092fa9c104a44bee81f121c8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-01-11 11:19:35 +0100

    Add position conversion functions
    Convert a cell coordinate to an index and the other way around

--- maze.el
+++ maze.el
@@ -1,5 +1,6 @@
 (require 'cl)
 (require 'dash)
+;;;; missing tests
 
 (cl-defstruct maze rows columns connections)
 
@@ -12,5 +13,29 @@
   (make-maze :rows (maze-rows source)
              :columns (maze-columns source)
              :connections (copy-hash-table (maze-connections source))))
+
+(defun maze/error-for-cell (maze cell)
+  (let ((column (car cell))
+        (row (cadr cell)))
+    (cond
+     ((< column 0) (format "Negative column (%s, %s)" column row))
+     ((< row 0) (format "Negative row (%s, %s)" column row))
+     ((>= column (maze-columns maze)) (format "Out of bound column (%s, %s)" column row))
+     ((>= row (maze-rows maze)) (format "Out of bound row (%s, %s)" column row)))))
+
+(defun maze/check-extremes (maze column row)
+  (let ((error-string (maze/error-for-cell maze (list column row))))
+    (if error-string
+        (error error-string))))
+
+(defun maze/position-to-index (maze column row)
+  (maze/check-extremes maze column row)
+  (+ column (* row (maze-columns maze))))
+
+(defun maze/index-to-position (maze index)
+  (maze/check-index maze index)
+  (let ((columns (maze-columns maze)))
+    (list (mod index columns)
+          (/ index columns))))
 
 (provide 'maze)
