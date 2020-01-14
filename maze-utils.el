e3504c6bd792e657c2141eb5c9e29e6ff5979369
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-01-14 09:02:31 +0100

    Extract useful functions from the Wilson implementation

--- maze-utils.el
+++ maze-utils.el
@@ -19,13 +19,13 @@
          (index (random size)))
     (nth index items)))
 
-(defun maze/--unoccupied-cells (maze exclusion-table)
+(defun maze/utils--unoccupied-cells (maze exclusion-table)
   (--filter (not (gethash it exclusion-table))
             (number-sequence 0 (1- (maze/get-cells-number maze)))))
 
 (defun maze/random-cell (maze &optional exclusion-table)
   (maze/random-choice
-   (maze/--unoccupied-cells maze (or exclusion-table (make-hash-table)))))
+   (maze/utils--unoccupied-cells maze (or exclusion-table (make-hash-table)))))
 
 (defun maze/left-cell (column row)
   (list (1- column) row))
@@ -64,6 +64,54 @@ The list has the :top :right :bottom :left keys and can be queried with assq"
                     (maze/left-cell column row)))))
 
 
+(defun maze/random-walk-step (maze current-index)
+  "Return a random position from the current one"
+  (let ((legal-moves (maze/valid-neighbors maze (maze/index-to-position maze current-index))))
+    (maze/position-to-index maze (maze/random-choice legal-moves))))
 
+(defun maze/utils--unfold-keep-going (next-value next-seed)
+  "Return value for unfold after a generic non visited cell has been added"
+  (cons next-value (cons next-seed nil)))
+
+(defun maze/utils--unfold-reached-visited-cell (next-value next-seed)
+  "Return value for unfold after a visited cell has been reached
+
+The next result will be the last"
+  (cons next-value (cons next-seed t)))
+
+(defun maze/utils--unfold-stop-after-next-insertion (next-value-to-insert)
+  "Add the current (visited) cell and then stop the next iteration"
+  (cons next-value-to-insert '(nil . nil)))
+
+(defun maze/utils--looped-path-unfold (maze stop-f next-cell-f current-index is-end-cell)
+  "Unfold a new looped path that connects a starting cell with one of (possibly many) visited cells"
+  (if current-index
+      (if is-end-cell
+          (maze/utils--unfold-stop-after-next-insertion current-index)
+        (let* ((next-position (funcall next-cell-f maze current-index))
+               (status (funcall stop-f maze current-index next-position)))
+          (cond
+           ((eq status :last)
+            (maze/utils--unfold-reached-visited-cell current-index next-position))
+           ((eq status :continue)
+            (maze/utils--unfold-keep-going current-index next-position))
+           ((eq status :stop) nil)
+           (t (error "Unsupported result from stop-f")))))))
+
+(defun maze/create-path (maze start stop-f next-cell-f)
+  "Return a list of indices representing a random path in the maze
+
+maze is the maze representing the path
+start is the starting cell (will always be included in the path)
+stop-p is a function that returns :stop :continue or :last
+next-cell-f is a function that returns the new cell from the maze and the index of the current one
+
+Returns a list of cells with at least one cell."
+  (append (-unfold  (lambda (seed)
+                      (maze/utils--looped-path-unfold maze
+                                                      stop-f
+                                                      next-cell-f
+                                                      (car seed) (cdr seed)))
+                    (cons start nil))))
 
 (provide 'maze-utils)
--- maze-wilson.el
+++ maze-wilson.el
@@ -14,68 +14,44 @@
                      (setq tmp (cons it tmp)))))
     (nreverse tmp)))
 
-(defun maze/--random-walk (maze current-index)
-  "Return a random position from the current one"
-  (let ((legal-moves (maze/valid-neighbors maze (maze/index-to-position maze current-index))))
-    (maze/position-to-index maze (maze/random-choice legal-moves))))
+(defun maze/wilson--next-path-cell (maze current-index)
+  (maze/random-walk-step maze current-index))
 
-(defun maze/--unfold-keep-going (next-value next-seed)
-  "Return value for unfold after a generic non visited cell has been added"
-  (cons next-value (cons next-seed nil)))
+(defun maze/wilson--generate-stop-f (used-cells)
+  (lexical-let ((used-cells used-cells))
+    (lambda (maze current-index next-candidate)
+      (if (gethash next-position used-cells)
+          :last
+        :continue))))
 
-(defun maze/--unfold-reached-visited-cell (next-value next-seed)
-  "Return value for unfold after a visited cell has been reached
-
-The next result will be the last"
-  (cons next-value (cons next-seed t)))
-
-(defun maze/--unfold-stop-after-next-insertion (next-value-to-insert)
-  "Add the current (visited) cell and then stop the next iteration"
-  (cons next-value-to-insert '(nil . nil)))
-
-(defun maze/--looped-path-unfold (maze used-cells current-index is-end-cell)
-  "Unfold a new looped path that connects a starting cell with one of (possibly many) visited cells"
-  (if current-index
-      (if is-end-cell
-          (maze/--unfold-stop-after-next-insertion current-index)
-        (let ((next-position (maze/--random-walk maze current-index)))
-          (if (gethash next-position used-cells)
-              (maze/--unfold-reached-visited-cell current-index next-position)
-            (maze/--unfold-keep-going current-index next-position))))))
-
-(defun maze/--looped-path (maze used-cells start)
-  (append (-unfold  (lambda (seed)
-                      (maze/--looped-path-unfold maze
-                                                 used-cells
-                                                 (car seed) (cdr seed)))
-                    (cons start nil))))
-
-(defun maze/--wilson-path (maze used-cells start)
+(defun maze/wilson--path (maze used-cells start)
   "Return a path starting from path and ending in a used-cell
 
 start must not be in the used-cells list"
   (if (gethash start used-cells)
       (error "Starting from an used cell!"))
-  (maze/wilson-erase-loops (maze/--looped-path maze used-cells start)))
+  (maze/wilson-erase-loops (maze/create-path maze start
+                                             (maze/wilson--generate-stop-f used-cells)
+                                             #'maze/wilson--next-path-cell)))
 
-(defun maze/--create-initial-exclusion-table (maze)
+(defun maze/wilson--create-initial-exclusion-table (maze)
     (let ((maze-size (maze/get-cells-number maze))
         (used-cells (make-hash-table)))
     (puthash (/ maze-size 2) t used-cells)
     used-cells))
 
-(defun maze/--carve-wilson (maze)
+(defun maze/wilson--carve (maze)
   (let ((new-maze maze)
-        (exclusion-table (maze/--create-initial-exclusion-table maze))
+        (exclusion-table (maze/wilson--create-initial-exclusion-table maze))
         (maze-size (maze/get-cells-number maze)))
     (while (< (hash-table-count exclusion-table) maze-size)
-      (let ((new-path (maze/--wilson-path new-maze exclusion-table (maze/random-cell new-maze exclusion-table))))
+      (let ((new-path (maze/wilson--path new-maze exclusion-table (maze/random-cell new-maze exclusion-table))))
         (--each (-partition-in-steps 2 1 new-path)
           (setq new-maze (maze/carve-passage-index new-maze (car it) (cadr it))))
         (--each new-path (puthash it t exclusion-table))))
     new-maze))
 
 (defun maze/wilson (columns rows)
-  (maze/--carve-wilson (maze/create-empty columns rows)))
+  (maze/wilson--carve (maze/create-empty columns rows)))
 
 (provide 'maze-wilson)
